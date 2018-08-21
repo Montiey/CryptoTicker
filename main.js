90cbb76e582ced6ca803f4754ea0eb2322850b8f
Jason Harriot <jasonharriot@gmail.com>
2018-08-21 00:26:43 -0400

    misc

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 var column = document.getElementById("column");
 
 function update(){
-	$.getJSON("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,BCH,ETH,LTC,XMR,DGC&tsyms=USD", function(data){
+	$.getJSON("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,BCH,ETH,LTC,XMR&tsyms=USD", function(data){
 		while(column.firstChild){	//wipe the column
 			column.removeChild(column.firstChild);
 		}
 		var maxDigits = 0;
 		var maxDecimals = 0;
 		
-		$.each(data, function(index, value){
+		$.each(data, function(index, value){	//figure out how much space the data will take up
 			maxDigits = Math.max(value.USD.toString().indexOf("."), maxDigits);
 			maxDecimals = Math.max((value.USD.toString().length - value.USD.toString().indexOf(".")) - 1, maxDecimals);
 		});
 		
-		console.log(maxDigits + ", " + maxDecimals);
 		$.each(data, function(index, value){
 			column.appendChild(newEntry(index, value.USD, maxDigits, maxDecimals));
 		});
 	}, "json");
-	console.log("update");
 }
 
 function newEntry(c, p, pr, tr){	//create a row (coin, price, preceding, trailing)
@@ -63,11 +61,9 @@ function normalizeFloat(value, preceding, trailing, symbol){	//fancy numbers
 	return str;
 }
 
-var interval = 10000;	//Set here so we can use even multiples later
-
 update();
-setInterval(update, interval);	//cryptocompare API updates every 10 seconds, so we might get 0 to 9.999 second old data
+setInterval(update, 10000);	//cryptocompare API updates every 10 seconds, so we might get 0 to 9.999 second old data
 
 setTimeout(function(){
 	window.location.reload(true);
-}, interval * 3); //reload instead of just update every 3 * 10 seconds = 30 seconds
+}, 1000 * 60 * 30); //reload instead of just update however often
