3f5bb8249ba942b237aa93ad560d575e00b53aeb
Jason Harriot <jasonharriot@gmail.com>
2018-08-20 20:35:09 -0400

    misc

--- main.js
+++ main.js
@@ -5,50 +5,61 @@ function update(){
 		while(column.firstChild){	//wipe the column
 			column.removeChild(column.firstChild);
 		}
+		var maxDigits = 0;
+		var maxDecimals = 0;
+		
 		$.each(data, function(index, value){
-			column.appendChild(newEntry(index, value.USD));
-		})
+			maxDigits = Math.max(value.USD.toString().indexOf("."), maxDigits);
+			maxDecimals = Math.max((value.USD.toString().length - value.USD.toString().indexOf(".")) - 1, maxDecimals);
+		});
+		
+		console.log(maxDigits + ", " + maxDecimals);
+		$.each(data, function(index, value){
+			column.appendChild(newEntry(index, value.USD, maxDigits, maxDecimals));
+		});
 	}, "json");
 	console.log("update");
 }
 
-function newEntry(c, p){	//create a row (coin, price)
+function newEntry(c, p, pr, tr){	//create a row (coin, price, preceding, trailing)
 	var elem = document.createElement("div");	//row entry container
 	var coin = document.createElement("div");	//left element
 	var price = document.createElement("div");	//right element
 	var subcoin = document.createElement("div");	//for centering
 	var subprice = document.createElement("div");	//^^
-	
+
 	elem.classList.add("row");
 	coin.classList.add("coin");
 	price.classList.add("price");
 	subcoin.classList.add("subcoin");
 	subprice.classList.add("subprice");
-	
-	subprice.textContent = "$" + normalizeFloat(p, 6, 6);
+
+	subprice.textContent = normalizeFloat(p, pr, tr, "$");
 	subcoin.textContent = c;
 
 	price.appendChild(subprice);
 	coin.appendChild(subcoin);
 	elem.appendChild(coin);
 	elem.appendChild(price);
-	
+
 	return elem;
 }
 
-function normalizeFloat(value, preceding, trailing){	//fancy numbers
+function normalizeFloat(value, preceding, trailing, symbol){	//fancy numbers
 	var str = value.toString();
 	if(str.indexOf(".") < 0){
 		str += ".";
 	}
 	var oldStr = str;
+	str = symbol + str;
 	for(var i = 0; i < preceding - oldStr.indexOf("."); i++){
 		str = "\xa0" + str;	//Normal spaces are ignored
 	}
 	for(var i = 0; i < trailing+1 - (oldStr.length - oldStr.indexOf(".")); i++){
-		str += "0";
+		str += "\xa0";
 	}
 
+	console.log(str);
 	return str;
 }
 
