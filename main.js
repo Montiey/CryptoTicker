3e7a874b36fe3b49952b4c1b0a85d0249e44182f
Jason Harriot <jasonharriot@gmail.com>
2018-08-23 19:19:25 -0400

    misc

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
-var column = document.getElementById("column");
+update();
+setInterval(update, 10000);	//cryptocompare API updates every 10 seconds, so we might get 0 to 9.999 second old data
+
+setTimeout(function(){
+	window.location.reload(true);
+}, 1000 * 60 * 30); //reload instead of just update however often
+
+getInfo();
 
 function update(){
+	var column = document.getElementById("column");
 	$.getJSON("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,BCH,ETH,LTC,XMR&tsyms=USD", function(data){
 		while(column.firstChild){	//wipe the column
 			column.removeChild(column.firstChild);
 		}
 		var maxDigits = 0;
 		var maxDecimals = 0;
-		
+
 		$.each(data, function(index, value){	//figure out how much space the data will take up
 			maxDigits = Math.max(value.USD.toString().indexOf("."), maxDigits);
 			maxDecimals = Math.max((value.USD.toString().length - value.USD.toString().indexOf(".")) - 1, maxDecimals);
 		});
-		
+
 		$.each(data, function(index, value){
 			column.appendChild(newEntry(index, value.USD, maxDigits, maxDecimals));
 		});
 	}, "json");
 }
 
-function newEntry(c, p, pr, tr){	//create a row (coin, price, preceding, trailing)
+function newEntry(c, p, pr, tr){	//create a row with given data
 	var elem = document.createElement("div");	//row entry container
 	var coin = document.createElement("div");	//left element
 	var price = document.createElement("div");	//right element
-	var subcoin = document.createElement("div");	//for centering
-	var subprice = document.createElement("div");	//^^
 
 	elem.classList.add("row");
 	coin.classList.add("coin");
 	price.classList.add("price");
-	subcoin.classList.add("subcoin");
-	subprice.classList.add("subprice");
 
-	subprice.textContent = normalizeFloat(p, pr, tr, "$");
-	subcoin.textContent = c + "\xa0";
-
-	price.appendChild(subprice);
-	coin.appendChild(subcoin);
+	price.textContent = normalizeFloat(p, pr, tr, "$");
+	coin.textContent = c + "\xa0";
+	
 	elem.appendChild(coin);
 	elem.appendChild(price);
 
@@ -46,7 +48,7 @@ function newEntry(c, p, pr, tr){	//create a row (coin, price, preceding, trailin
 function normalizeFloat(value, preceding, trailing, symbol){	//fancy numbers
 	var str = value.toString();
 	if(str.indexOf(".") < 0){
-		str += ".0";	//The string will always be sane, or at least end with this
+		str += ".0";	//The return value shall always be in a sane format
 	}
 	var oldStr = str;
 	str = symbol + str;
@@ -56,14 +58,20 @@ function normalizeFloat(value, preceding, trailing, symbol){	//fancy numbers
 	for(var i = 0; i < trailing+1 - (oldStr.length - oldStr.indexOf(".")); i++){
 		str += "\xa0";
 	}
-
-//	console.log(str);
 	return str;
 }
 
-update();
-setInterval(update, 10000);	//cryptocompare API updates every 10 seconds, so we might get 0 to 9.999 second old data
-
-setTimeout(function(){
-	window.location.reload(true);
-}, 1000 * 60 * 30); //reload instead of just update however often
+function getInfo(){	//102% certified SO blackmagic
+	window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+	var pc = new RTCPeerConnection({iceServers:[]}), noop = function(){};
+	pc.createDataChannel('');
+	pc.createOffer(pc.setLocalDescription.bind(pc), noop);
+	pc.onicecandidate = function(ice){
+		if (ice && ice.candidate && ice.candidate.candidate){
+			var ip = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+			pc.onicecandidate = noop;
+			document.getElementById("ip").innerHTML = ip;
+		}
+	};
+	/*document.getElementById("url").innerHTML = document.URL;*/
+}
